62b843c4465f73da3287edd2d240ecfd6c80345e
Colin Cross <ccross@google.com>
2023-12-04 10:31:13 -0800

    aoc04

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -5,34 +5,50 @@ use std::fs::read_to_string;
 
 #[derive(Debug)]
 struct Card {
-    id: u32,
-    winning: HashSet<u32>,
-    have: HashSet<u32>,
+    id: usize,
+    winning: HashSet<usize>,
+    have: HashSet<usize>,
 }
 
 impl Card {
     fn new(s: &str) -> Self {
         let re = Regex::new(r"Card\s+(?P<id>\d+): (?P<winning>[\d\s]+) \| (?P<have>[\d\s]+)").unwrap();
         let caps = re.captures(s).unwrap();
-        let id: u32 = caps.name("id").unwrap().as_str().parse().unwrap();
-        let winning: HashSet<u32> = caps.name("winning").unwrap().as_str()
+        let id: usize = caps.name("id").unwrap().as_str().parse().unwrap();
+        let winning: HashSet<usize> = caps.name("winning").unwrap().as_str()
             .split_whitespace()
-            .map(|s| s.parse::<u32>().unwrap())
+            .map(|s| s.parse::<usize>().unwrap())
             .collect();
-        let have: HashSet<u32> = caps.name("have").unwrap().as_str()
+        let have: HashSet<usize> = caps.name("have").unwrap().as_str()
             .split_whitespace()
-            .map(|s| s.parse::<u32>().unwrap())
+            .map(|s| s.parse::<usize>().unwrap())
             .collect();
         
         Self{id, winning, have}
     }
 
-    fn value(&self) -> u32 {
+    fn count(&self) -> usize {
+        self.winning.intersection(&self.have).count()
+    }
+
+    fn value(&self) -> usize {
         match self.winning.intersection(&self.have).count() {
             0 => 0,
             n => 1 << (n-1),
         }
     }
+}
+
+fn count_recursive_cards(cards: &[Card], all_cards: &Vec<Card>) -> usize {
+    let mut count = cards.len(); // count these cards
+    for c in cards.iter() {
+        let v = c.count();
+        if v > 0 {
+            let extra_cards = &all_cards[c.id..c.id+v];
+            count += count_recursive_cards(extra_cards, all_cards);
+        }
+    }
+    count
 }
 
 fn main() {
@@ -45,5 +61,7 @@ fn main() {
         .map(Card::new)
         .collect();
 
-    println!("{}", cards.iter().map(|c| c.value()).sum::<u32>());
+    println!("{}", cards.iter().map(|c| c.value()).sum::<usize>());
+
+    println!("{}", count_recursive_cards(&cards, &cards));
 }
