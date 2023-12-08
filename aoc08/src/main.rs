712ef6711bd1ed441eae45af37b6ce7cb0b53c35
Colin Cross <ccross@google.com>
2023-12-08 10:28:37 -0800

    aoc08-2

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
-use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::env;
 use std::fs::read_to_string;
 
@@ -43,12 +43,69 @@ fn main() {
         .map(to_node)
         .collect();
 
-    let mut node = ['A', 'A', 'A'];
+    let mut positions: Vec<Node> =
+        nodes
+        .keys()
+        .filter(|node| node[2] == 'A')
+        .map(|node| *node)
+        .collect();
+
     let mut count: usize = 0;
-    while node != ['Z', 'Z', 'Z'] {
-        node = nodes[&node][directions[count % directions.len()]];
-        count += 1;
+
+    let mut starts: Vec<usize> = Vec::new();
+    let mut loop_lengths: Vec<usize> = Vec::new();
+    let mut z_offsets_in_loop: Vec<usize> = Vec::new();
+    
+    for start_position in positions.iter() {
+        let mut node = start_position;
+        let mut count = 0;
+        let mut order: Vec<Node> = Vec::new();
+        let mut seen: HashMap<(Node, usize), usize> = HashMap::new(); 
+        let mut z_position = 0;
+
+        loop {
+            if node[2] == 'Z' {
+                z_position = count;
+            }
+            let entry = seen.entry((*node, count % directions.len()));
+            match entry {
+                Entry::Occupied(x) => { 
+                    break;
+                }
+                Entry::Vacant(x) => {
+                    x.insert(count);
+                }
+            }
+            order.push(*node);
+            node = &nodes[node][directions[count % directions.len()]];
+            count += 1;
+        }
+
+        let loop_start = seen[&(*node, count % directions.len())];
+
+        dbg!(count, z_position);
+        starts.push(loop_start);
+        loop_lengths.push(count - loop_start);
+        z_offsets_in_loop.push(z_position - loop_start);
+        //dbg!(loop_start, &order);
+
+        assert!(z_position == count - loop_start);
+    }
+    dbg!(&starts, &loop_lengths, &z_offsets_in_loop);
+
+   
+    let mut counts: Vec<usize> = starts.iter().zip(z_offsets_in_loop.iter()).map(|(a, b)| a+b).collect();
+
+    //let mut counts: Vec<usize> = vec![counts2[0]];
+    
+    while counts.iter().min() != counts.iter().max() {
+        let min = counts.iter().enumerate().min_by_key(|(_, count)| *count).unwrap().0;
+        let max = counts.iter().enumerate().max_by_key(|(_, count)| *count).unwrap().0;
+
+        assert!(counts[min] < counts[max]);       
+        //dbg!(min, counts[min], max, counts[max]);
+        counts[min] += loop_lengths[min];
     }
 
-    println!("{}", count);
+    dbg!(&counts);
 }
