45c051411aaa39def50c108c97665182f341226d
Colin Cross <ccross@google.com>
2023-12-15 09:15:06 -0800

    aoc15

--- aoc15/src/main.rs
+++ aoc15/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::read_to_string;
+
+fn hash(s: &str) -> usize {
+    s
+        .as_bytes()
+        .iter()
+        .fold(0, |hash, &c| ((hash + c as usize) * 17) % 256)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = args[1].as_str();
+    let data = read_to_string(&filename).unwrap();
+
+    let line = data.lines().next().unwrap();
+
+    let steps = line.split(",").collect::<Vec<_>>();
+
+    println!("{}",
+             steps
+                 .iter()
+                 .map(|&s| hash(s))
+                 .sum::<usize>());
 }
